bcbd62d88105d32b725174477307154af9a64786
unknown <jensennics.dlrs@gmail.com>
2023-01-14 11:56:08 +0800

    Defined function that will get the sum of all the elements in the binary tree.

--- exer1_binary_search_tree.py
+++ exer1_binary_search_tree.py
@@ -73,6 +73,11 @@ class BinarySearchTreeNode:
             return self.data
         return self.right.find_max()
 
+    def calculate_sum(self):
+        left_sum = self.left.calculate_sum() if self.left else 0
+        right_sum = self.right.calculate_sum() if self.right else 0
+        return self.data + left_sum + right_sum
+
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
 
