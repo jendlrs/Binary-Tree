f6e97b66c0c953afb6d5ad6729174a52e242a603
unknown <jensennics.dlrs@gmail.com>
2023-01-14 13:20:34 +0800

    Defined delete function and added various conditions.

--- binary_search_tree2.py
+++ binary_search_tree2.py
@@ -66,6 +66,19 @@ class BinarySearchTreeNode:
         if self.left is None:
             return self.data
         return self.left.find_min()
+    
+    def delete (self, val):
+        if val < self.data:
+            if self.left:
+                self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            if self.left is None:
+                return self.right
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
