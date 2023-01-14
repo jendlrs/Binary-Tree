524e2fffc5399d8ed4411635ee428fc11f1500e9
unknown <jensennics.dlrs@gmail.com>
2023-01-14 14:17:10 +0800

    Added again the class BinarySearchTreeNode and added init method under the class.

--- binary_search_treeSW.py
+++ binary_search_treeSW.py
@@ -1,1 +1,7 @@
 #Create a demo using the letters in your fullname as the content of the binary tree.
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
