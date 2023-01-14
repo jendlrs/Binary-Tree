4d1ab232ec2b73a67a60bac6ffe1dda10ce8ed97
unknown <jensennics.dlrs@gmail.com>
2023-01-14 10:43:55 +0800

    Defined function to build tree. It includes for loop to add datthe root by using the add child function.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -39,6 +39,14 @@ class BinarySearchTreeNode:
 
         return elements
 
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+    
+    return root
+
 if __name__ == '__main__':
     numbers = [17, 4, 1, 20, 9, 23, 18, 34]
 
