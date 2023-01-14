472b273ce5c8361a203bf095a082b9075b7695d9
unknown <jensennics.dlrs@gmail.com>
2023-01-14 12:47:56 +0800

    Added add_child function to thw project, just like on the firstbinary-search-tree project, this function will check the values.

--- binary_search_tree2.py
+++ binary_search_tree2.py
@@ -4,4 +4,21 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
-        
+    def add_child (self, data):
+        #If the value already exist
+        if data == self.data:
+            return 
+
+        #If data is less than the value of current node --Add to left subtree
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data) 
+            else:
+                self.left = BinarySearchTreeNode(data)
+
+        #If data is greater than the value of current node --Add to right subtree
+        else:
+            if self.right:
+                self.right.add_child(data) 
+            else:
+                self.right = BinarySearchTreeNode(data)
