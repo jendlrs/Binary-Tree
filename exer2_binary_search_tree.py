c4daba5fe4b976c7f4d0a172f320550ef4bc72f5
unknown <jensennics.dlrs@gmail.com>
2023-01-14 13:52:23 +0800

    Tried building again the tree and deleting 9 if it will work.

--- exer2_binary_search_tree.py
+++ exer2_binary_search_tree.py
@@ -108,3 +108,8 @@ if __name__ == '__main__':
     print("Building tree with these elements:", numbers)
     print("After deleting 20", numbers_tree.in_order_traversal())
 
+    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree.delete(9)
+    print("After deleting 9 ",numbers_tree.in_order_traversal())
+
+
