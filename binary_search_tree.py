27585358c37b25838eec5dfda7b21e0c02fc266a
unknown <jensennics.dlrs@gmail.com>
2023-01-14 10:32:37 +0800

    Defined function in order traversal to print all the elements. Also added the condition to visit the left tree.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -22,3 +22,17 @@ class BinarySearchTreeNode:
                 self.right.add_child(data) 
             else:
                 self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+
+        #Visit the left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        #Visit base node
+        elements.append(self.data)
+
+        #Visit the right tree
+
+        return elements
