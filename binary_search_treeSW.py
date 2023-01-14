6b9ec5a290a4381c0470f7063d8dadc4404dd818
unknown <jensennics.dlrs@gmail.com>
2023-01-14 14:40:40 +0800

    Added in order traversal function

--- binary_search_treeSW.py
+++ binary_search_treeSW.py
@@ -24,3 +24,19 @@ class BinarySearchTreeNode:
                 self.right.add_child(data) 
             else:
                 self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+
+        #Visit the left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+        
+        #Visit base node
+        elements.append(self.data)
+
+        #Visit the right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
