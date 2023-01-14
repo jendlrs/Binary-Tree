f4342869706071fbdb4ea2bd4a0ae25337a0fc9e
unknown <jensennics.dlrs@gmail.com>
2023-01-14 13:47:21 +0800

    Now, I pasted the code from binary_search_tree2 that will be modified in the next commits.

--- exer2_binary_search_tree.py
+++ exer2_binary_search_tree.py
@@ -1,3 +1,109 @@
 #Modify delete method in class BinarySearchTreeNode class to use min element from left subtree. 
 #You will remove lines marked with ---> and use max value from left subtree
 
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child (self, data):
+        #If the value already exist
+        if data == self.data:
+            return 
+
+        #If data is less than the value of current node --Add to left subtree
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data) 
+            else:
+                self.left = BinarySearchTreeNode(data)
+
+        #If data is greater than the value of current node --Add to right subtree
+        else:
+            if self.right:
+                self.right.add_child(data) 
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def search (self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            #value might be in left subtree
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False #It means the value does not exist in the elements
+
+        if val > self.data:
+            #value might be in right subtree
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False #It means the value does not exist in the elements
+
+    def in_order_traversal(self):
+        elements = []
+
+        #Visit the left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+        
+        #Visit base node
+        elements.append(self.data)
+
+        #Visit the right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+    
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+    
+    def delete (self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.left
+
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete(min_val)
+
+        return self
+
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+    
+    return root    
+
+if __name__ == '__main__':
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree (numbers)
+    numbers_tree.delete(9)
+
+    print("Building tree with these elements:", numbers)
+    print("After deleting 9", numbers_tree.in_order_traversal())
